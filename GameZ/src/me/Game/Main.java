c3eef3f727f77759074bb8107470cacf0a6b9e1f
Feilax <loerden.s3@gmail.com>
2014-01-09 17:21:40 +0100

    Frame added

--- GameZ/src/me/Game/Main.java
+++ GameZ/src/me/Game/Main.java
@@ -1,5 +1,12 @@
 package me.Game;
 
 public class Main {
+    Frame frame = new Frame();
+    frame.setSize(1020, 600);
+	   frame.setVisible(true);
+    frame.setResizable(false);
+    frame.setLocationRelativeTo(null);
+    frame.setDefaultCloseOperation(3);
 
+    frame.initialize();
 }
