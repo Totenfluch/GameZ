7c1db0aeac83fbc688c3f2befcf378cd8cd135e2
Totenfluch <ziegler.christian@kabelmail.de>
2014-01-10 18:23:01 +0100

    improved design of LoginWindow

--- GameZ/src/me/security/LoginWindow.java
+++ GameZ/src/me/security/LoginWindow.java
@@ -3,8 +3,9 @@ package me.security;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,14 +13,16 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+import me.Game.Main;
 import me.Other.*;
 
 public class LoginWindow extends JFrame{
 	private JTextField Username;
 	private JPasswordField Password;
-	private JButton login;
+	private JButton loginbutton;
 	private JButton overridelogin;
 	private JButton createAccount;
+	private JLabel or;
 	
 	public LoginWindow(){
 		setSize(800, 600);
@@ -43,6 +46,56 @@ public class LoginWindow extends JFrame{
 		Password.setBounds(259, 297, 249, 53);
 		Password.setFont(new Font("Serif", Font.BOLD, 30));
 		background.add(Password);
+		
+		// Replace mim image - der standart button ist hart schwul
+		loginbutton = new JButton("   Login   ");
+		loginbutton.setBounds(300, 390, 170, 30);
+		loginbutton.setForeground(Color.BLACK);
+		loginbutton.setFont(new Font("Serif", Font.BOLD, 20));
+		background.add(loginbutton);
+		
+		if(Main.devbuild == true){
+			overridelogin = new JButton("Override Login");
+			overridelogin.setBounds(300, 440, 170, 30);
+			overridelogin.setFont(new Font("Serif", Font.BOLD, 20));
+			background.add(overridelogin);
+		}else{
+			or = new JLabel("or");
+			or.setForeground(Color.WHITE);
+			or.setBounds(370, 420, 170, 30);
+			or.setFont(new Font("Serif", Font.BOLD, 30));
+			background.add(or);
+			
+			createAccount = new JButton("Create Account");
+			createAccount.setBounds(300, 460, 170, 30);
+			createAccount.setFont(new Font("Serif", Font.BOLD, 20));
+			background.add(createAccount);
+		}
+		
+		thehandler handler = new thehandler();
+		loginbutton.addActionListener(handler);
+		overridelogin.addActionListener(handler);
+		createAccount.addActionListener(handler);
 		 
+	}
+	
+	private class thehandler implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if(event.getSource() == loginbutton){
+				
+			}
+			
+			if(event.getSource() == overridelogin){
+				
+			}
+			
+			if(event.getSource() == createAccount){
+				
+			}
+		
+		}
+		
 	}
 }
