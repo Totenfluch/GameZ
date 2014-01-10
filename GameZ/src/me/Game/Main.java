9d2729efcb08737584c070b3918e66e2e5241c5c
Totenfluch <ziegler.christian@kabelmail.de>
2014-01-10 15:24:48 +0100

    changed defaultCloseOperation ( Test Push )

--- GameZ/src/me/Game/Main.java
+++ GameZ/src/me/Game/Main.java
@@ -1,22 +1,24 @@
 package me.Game;
 
+import javax.swing.JFrame;
+
 public class Main 
 {
 	public static void main(String [ ] args)
 	{
-     Frame frame = new Frame();
-     frame.setSize(1020, 600);
-     frame.setVisible(true);
-     frame.setResizable(false);
-     frame.setLocationRelativeTo(null);
-     frame.setDefaultCloseOperation(3);
+		Frame frame = new Frame();
+		frame.setSize(1020, 600);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-     frame.initialize();
-    
-     while(true)
-     {
- 	    frame.Update();
- 	    frame.Repaint();
-     }
+		frame.initialize();
+
+		while(true)
+		{
+			frame.Update();
+			frame.Repaint();
+		}
 	}
 }
